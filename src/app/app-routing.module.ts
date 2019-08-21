bde8c60fddb28ed903cfe3181766bf7ab7d1bad5
ingsistemavzla <Ingenierosdesistemavenezuela@gmail.com>
2019-08-20 21:34:18 -0400

    welcome routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
 import { QuestionsComponent } from './components/questions/questions.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 
 const routes: Routes = [
-
+  {path:'',component: WelcomeComponent},
   {path: 'questions',component: QuestionsComponent},
   {path: 'form', component: FormComponent}
 ];
